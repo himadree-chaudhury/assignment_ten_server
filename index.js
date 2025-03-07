5767ec9f2cb20d0d4de92ef43a5a53a1a1e5a919
himadree-chaudhury <himchy2001@gmail.com>
2025-03-07 15:34:20 +0600

    server setup & running

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
-
 const { MongoClient, ServerApiVersion } = require("mongodb");
+const port = process.env.PORT || 5000;
 
+const app = express();
 
+app.use(cors());
+app.use(express.json());
 
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lhej2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lhej2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -19,7 +23,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -27,7 +31,15 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("This is a movie portal server");
+});
+
+app.listen(port, () => {
+  console.log(`Movie server is running in port : ${port}`);
+});
