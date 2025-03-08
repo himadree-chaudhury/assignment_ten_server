666db2a929a2ff379d3f094d6366ec4d5544c2ab
himadree-chaudhury <himchy2001@gmail.com>
2025-03-09 00:06:44 +0600

    single movie get

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -37,13 +37,17 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
+    app.get("/movies/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await movieCollection.findOne(query);
+      res.send(result);
+    });
     app.get("/movies/featured", async (req, res) => {
       const cursor = movieCollection.find().sort({ Rating: -1 }).limit(6);
       const result = await cursor.toArray();
       res.send(result);
     });
-
-
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -54,7 +58,6 @@ run().catch(console.dir);
 app.get("/", (req, res) => {
   res.send("This is a movie portal server");
 });
-
 
 app.listen(port, () => {
   console.log(`Movie server is running in port : ${port}`);
